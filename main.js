f600052e1b6335d35273d5c742d0da58ddc1e82a
jobayed24 <jkhouse117@gmail.com>
2023-07-01 15:26:13 +0600

    get collection of p tags and it behvae like array so accessing the p tage i use numeric indexing

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-document.getElementById("demo").innerHTML="Jobayed";
+const elements=document.getElementsByTagName("p");
+
+document.getElementById("demo").innerHTML="this is the first Pharagrap <br>"+elements[1].innerHTML;
+
+
+
+
+console.log(elements);
