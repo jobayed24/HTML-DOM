84c0fb2b36ff2a92822b2244e8c89bef5bd74c8e
jobayed24 <jkhouse117@gmail.com>
2023-07-02 11:18:34 +0600

    all needed anchors are printed in documents

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
-const fValues=document.forms["frm1"];
+const mydoc=document;
+
+const myAnchors=mydoc.anchors.length;
 
 
 
-let x = fValues.length;
 
-let text="";
-
-for(let i=0; i<x; i++){
-    text+=fValues.elements[i].value+"<br/ ><br />";
-}
-document.getElementById("demo").innerHTML=text;   
-
-
-console.log(text);
-
-
+document.getElementById("demo").innerHTML=myAnchors;
