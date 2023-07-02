fbf5dd24e1d2daeae47b6c4d85f6ef39118fd936
jobayed24 <jkhouse117@gmail.com>
2023-07-02 14:33:58 +0600

    when click on document then change html body background color

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-const newEvent=document.addEventListener("click",function(){
-    myFunction(p1,p2);
-});
-
-let p1, p2;
-p1=5;
-p2=6;
-
-function myFunction(a,b){
-    result=a*b;
-
-    document.getElementById("demo").innerHTML=result;
-}
+document.addEventListener("click",function(){
+    document.body.style.backgroundColor="blue";
+})
