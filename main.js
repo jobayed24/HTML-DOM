fa2dd53ba13ed11826986f839ae7f0aa4e5859d8
jobayed24 <jkhouse117@gmail.com>
2023-07-02 17:01:03 +0600

    create attributes and add value and set attributesNode and change value of h1 tage

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 function myFunction(){
-    const newWindow=window.open();
+ //create attrubutes
+    const att=document.createAttribute("class");
 
-    newWindow.document.open();
-    newWindow.document.write("<h2>Hey, Hello</h2>");
-    newWindow.document.write("<p>This is jobayed <p>");
-    newWindow.document.close();
- 
+    att.value="demoClass";
+
+    let ld=document.getElementsByTagName('h1')[0].setAttributeNode(att);
    
 
-   
 }
