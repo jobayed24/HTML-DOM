09d22b2125332a301ba76fae90a2945a5888b7a7
jobayed24 <jkhouse117@gmail.com>
2023-07-03 10:47:59 +0600

    Problem :solved

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const newbutton=document.createAttribute("onclick");
 function hmm(){
     document.getElementById("myDIV").innerHTML="This is explore new things ";
 }
-newbutton.value="somethings";
+newbutton.value="hmm()";
 
 
 document.getElementsByTagName("button")[0].setAttributeNode(newbutton);
