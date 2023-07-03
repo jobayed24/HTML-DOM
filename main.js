e08d0ee8334509333945ab22a2193cb184313610
jobayed24 <jkhouse117@gmail.com>
2023-07-03 10:42:29 +0600

    Problem: virtually create button and when click button the element will change :Not Solve Yet

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-const para=document.createElement('p');
-
-para.innerText="This is a Pharagrahp";
-
+const para=document.createElement("button");
+para.innerText="Hello Button";
 document.getElementById("myDIV").appendChild(para);
+
+const newbutton=document.createAttribute("onclick");
+function hmm(){
+    document.getElementById("myDIV").innerHTML="This is explore new things ";
+}
+newbutton.value="somethings";
+
+
+document.getElementsByTagName("button")[0].setAttributeNode(newbutton);
+
+console.log(newbutton);
