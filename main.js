41351627b68d50e5c5c688711464a19c2edd175a
jobayed24 <jkhouse117@gmail.com>
2023-07-05 13:40:37 +0600

    childelementcount and children.length method are the same value return

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-const getImgID=document.getElementById("myImg").attributes;
+const lwcountchild=document.getElementById("myDIV").childElementCount;
 
+document.getElementById("demo").innerHTML=lwcountchild;
 
-let text="";
-
-for( let i=0;i<getImgID.length; i++){
-    text+=getImgID[i].name+" : "+getImgID[i].nodeValue+"<br />";
-}
-
-document.getElementById("demo").innerHTML=text;
-console.log(text);
