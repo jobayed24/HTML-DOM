5ded7bc0353bddcdb71ff26d0b643a9cd14625be
jobayed24 <jkhouse117@gmail.com>
2023-08-24 00:24:04 +0600

    onchange works only html select option

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
-const getInputValue=document.getElementById("finput");
+let getSelectValue=document.getElementById("mySelect");
 
-getInputValue.onblur=function(){myOnBlure()}
 
-function myOnBlure(){
-  if(getInputValue.value==""){
-    alert("Field is empty")
-    getInputValue.style.backgroundColor="red";
-    getInputValue.value="Put Something here";
-  }else{
-    getInputValue.value=getInputValue.value.toUpperCase();
-    getInputValue.style.backgroundColor="green";
-    getInputValue.style.color="white";
-  }
+getSelectValue.addEventListener("change",selectDataChange);
+
+function selectDataChange(){
+  document.getElementById("demo").innerHTML="You are selected "+getSelectValue.value;
 }
