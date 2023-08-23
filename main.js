c957c94fd4649050a11f390c64ec824ad2b96d44
jobayed24 <jkhouse117@gmail.com>
2023-08-23 16:53:51 +0600

    print mehtod use to before and afterprint

--- main.js
+++ main.js
@@ -1,35 +1,10 @@
 
-// getMyDiv=document.getElementsByTagName("div");
-
-// getMyDiv.click=function(){myFunction};
-
-
-// function myFunction(){
-//   getMyDiv.style.animation="mymove 5s 2"
-// }
-
-getMyDiv = document.getElementById("divAnimation");
-
-
-function myFunction(){
-  getMyDiv.style.animation="mymove 5s 2";
+window.onbeforeprint=function(){myBeforePrint()}
+window.onafterprint=function(){myAfterPrint()}
+function myBeforePrint(){
+  alert("Are you sure to Printing");
+}
+function myAfterPrint(){
+  alert("Thank you for Printing");
 }
 
-getMyDiv.addEventListener("animationstart",myAnimationStart);
-
-function myAnimationStart(){
-  this.innerHTML="changing addEvent Listner";
-  this.style.backgroundColor="green";
-}
-
-getMyDiv.addEventListener("animationiteration",myAnimationIteration);
-function myAnimationIteration(){
-  this.innerHTML="text changing";
-  this.style.backgroundColor="yellow";
-}
-
-getMyDiv.addEventListener("animationend",myAnimationEnd);
-function myAnimationEnd(){
-  this.innerHTML="Animation has finished";
-  this.style.backgroundColor="blue";
-}
