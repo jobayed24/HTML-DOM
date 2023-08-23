5756ed0e2d05d6691beb1e45d17db528f443cf8f
jobayed24 <jkhouse117@gmail.com>
2023-08-23 17:11:03 +0600

    small project for input section

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+const getInputValue=document.getElementById("finput");
 
-window.onbeforeunload=function(){ return myBeforeUnload()}
+getInputValue.onblur=function(){myOnBlure()}
 
-function myBeforeUnload(){
-  return("your data may be not save");
+function myOnBlure(){
+  if(getInputValue.value==""){
+    alert("Field is empty")
+    getInputValue.style.backgroundColor="red";
+    getInputValue.value="Put Something here";
+  }else{
+    getInputValue.value=getInputValue.value.toUpperCase();
+    getInputValue.style.backgroundColor="green";
+    getInputValue.style.color="white";
+  }
 }
