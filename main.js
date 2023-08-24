8c6647ea94d5be543e48393c4c679bef5e85b856
jobayed24 <jkhouse117@gmail.com>
2023-08-24 22:23:14 +0600

    open new window and write in a documents

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let getButton=document.getElementById("getButton");
 
 getButton.addEventListener('click',function(){
-  document.open();
-document.write("<h1>Hey Jobayed hossen. What's up? ");
-document.write("<p>This is second line of documents where data i have write down</p>")
-document.close();
+ let myDocument= window.open();
+ myDocument.document.write("<h1>This is new Documents</h1>");
+ myDocument.document.write("<p>This is new Pharagraph</p>");
+ myDocument.document.close();
 })
