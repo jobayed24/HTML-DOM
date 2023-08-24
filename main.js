430585a417219e668db56d8310726243dcca24fa
jobayed24 <jkhouse117@gmail.com>
2023-08-24 20:37:12 +0600

    body content changed by addeleventlistener

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-let getBodyElement=document.getElementsByTagName("body")[0];
+let bodyConetnet=document.getElementsByTagName("h1")[0];
+let newUppercase=bodyConetnet.innerHTML.toUpperCase();
 
-getBodyElement.addEventListener('click',getTagName);
-
-function getTagName(){
-  let getInput=document.activeElement.tagName;
-  document.getElementById("demo").innerHTML=getInput;
-
+bodyConetnet.style.backgroundColor="red";
+bodyConetnet.addEventListener('mouseover',removerBackground);
+function removerBackground(){
+  bodyConetnet.style.backgroundColor="transparent";
+  bodyConetnet.style.cursor="pointer";
 }
 
-// console.log(getBodyElement);
+
+console.log(newUppercase);
