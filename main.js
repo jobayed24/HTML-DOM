8eb5d19d4af7c2c0295009c0bab44a16127716ce
jobayed24 <jkhouse117@gmail.com>
2023-08-27 00:50:02 +0600

    make a small project with cookie and prompt

--- main.js
+++ main.js
@@ -1,44 +1,47 @@
-function setCookie(cname,cvalue,exdays){
+function setCookie(cname,cvalue,cdays){
     let d=new Date();
-    d.setTime(d.getTime()+(exdays*24*60*60*100))
-    let expires="expires="+d.toUTCString();
-    document.cookie=(cname+"="+cvalue+";"+expires)
+    d.setTime(d.getTime()+(cdays*24*60*60*100));
+    let expires=d.toUTCString();
+    document.cookie=cname+"="+cvalue+";expires="+expires+";path=/"
 }
 
+// document.cookie='name=;username=;expires=path=/'
 let myBrowserCookie=document.cookie;
-
-console.log(document.cookie);
-
+// console.log(myBrowserCookie);
 function getCookie(cname){
     let name=cname+"=";
+    if(myBrowserCookie!=""){
     let ca=myBrowserCookie.split(";");
-    for(let i=0; i<ca.length; i++){
-        let c=ca[i];
-        
-        
-        while(c.charAt(0)==" "){
-            c=c.substring(1);
-        }
-        if(c.indexOf(name)=="0"){
-        return c.substring(name.length,c.length);
-        }
-        
-    }
-    return ""
     
-}
-
-function checkCookie(cdata){
-    let user=getCookie(cdata)
-    console.log(user);
-    if(user!=""){
-        alert("Welcome again "+user);
-    }
-    else{
-        user=prompt("write your name","");
-        if(user!=null && user!=""){
-            setCookie(cdata,user,1);
+    for(let i=0;i<ca.length;i++){
+        let sc=ca[i];
+        while(sc.charAt(0)==" "){
+            sc=sc.substring(1);
+        }
+        if(sc.indexOf(name)==0){
+          return  sc=sc.substring(name.length,sc.length);
         }
     }
 }
+    return '';
+}
 
+function checkCookie(cvalue){
+    let getMyCookie=getCookie(cvalue);
+    if(getMyCookie!=""){
+        alert("Hey "+getMyCookie)
+    }else if(getMyCookie=="" || getMyCookie==null){
+       let ccname= prompt("Write your cookie name")
+       
+       if(ccname!="" && ccname!=null){
+        let ccvalue=prompt("Write your cookie value")
+        if(ccvalue!="" && ccvalue!=null){
+           
+            setCookie(ccname,ccvalue,1)
+        }
+       }else{
+        alert("Cookie name must be needed");
+       }
+        
+    }   
+}
