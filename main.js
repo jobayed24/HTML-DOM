ef9e4bf482acc04744a6195a54bbc927fb76985e
jobayed24 <jkhouse117@gmail.com>
2023-08-26 22:10:55 +0600

    workin with cookie

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
-let getButton=document.getElementById("getButton");
-
-getButton.addEventListener('click',function(){
-let person=prompt("Write your name","Jobayed");
-
-let x;
-if(person==null || person==""){
-    x= "You cancelled";
-}else{
-    x="Welcome "+person+" We are here to Served for you ";
+function setCookie(cname,cvalue,exdays){
+    let d=new Date();
+    d.setTime(d.getTime()+(exdays*24*60*60*100))
+    let expires="expires="+d.toUTCString();
+    document.cookie=(cname+"="+cvalue+";"+expires)
 }
-document.getElementById("demo").innerHTML=x;
-})
+
+let myBrowserCookie=document.cookie;
+
+console.log(document.cookie);
+
+function getCookie(cname){
+    let name=cname+"=";
+    let ca=myBrowserCookie.split(";");
+    for(let i=0; i<ca.length; i++){
+        let c=ca[i];
+        
+        
+        while(c.charAt(0)==" "){
+            c=c.substring(1);
+        }
+        if(c.indexOf(name)=="0"){
+        return c.substring(name.length,c.length);
+        }
+        
+    }
+    return ""
+    
+}
+
+function checkCookie(cdata){
+    let user=getCookie(cdata)
+    console.log(user);
+    if(user!=""){
+        alert("Welcome again "+user);
+    }
+    else{
+        user=prompt("write your name","");
+        if(user!=null && user!=""){
+            setCookie(cdata,user,1);
+        }
+    }
+}
+
