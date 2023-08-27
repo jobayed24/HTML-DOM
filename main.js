594c334f29f4300ff7e762831539c6e2fc623957
jobayed24 <jkhouse117@gmail.com>
2023-08-28 02:03:51 +0600

    take body tag and append it p tag

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-function getActive(){
- let mywind=window.open()
- mywind.document.write("Jk")
- setTimeout(mywindfn,3000)
- function mywindfn(){
-  mywind.close();
- }
+let getBodyTag=document.body;
 
+let createPElement=document.createElement("p");
+let addText=document.createTextNode("Hey this is jobayed");
+
+createPElement.appendChild(addText);
+
+function addPElemnets(){
+  getBodyTag.appendChild(createPElement);
 }
+
+console.log(getBodyTag)
