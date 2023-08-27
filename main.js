84c468e9b5244039673b64bf1e65fd4a799f47cc
jobayed24 <jkhouse117@gmail.com>
2023-08-27 20:39:27 +0600

    make animation with js

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
-function cancelMytimeOut(){
-    clearInterval(mysetInterval)
+function makeAnimate(){
+  const getAnimate=document.getElementById('animate');
+  const getRightAnimate=document.getElementById("righAnimate");
+  let righID=null;
+  let rightPos=0;
+
+  clearInterval(righID);
+
+  rightID=setInterval(righAnimate,5);
+  function righAnimate(){
+    if(rightPos==450){
+      clearInterval(righID)
+    }else{
+      
+      rightPos++;
+      getRightAnimate.style.top=rightPos+"px";
+      getRightAnimate.style.right=rightPos+"px"
+    }
+  }
+
+
+
+  let id=null;
+  let pos=0;
+  clearInterval(id);
+  id=setInterval(myMove,5);
+  function myMove(){
+    if(pos==450){
+      clearInterval(id)
+    }else{
+      pos++;
+      getAnimate.style.top=pos+"px";
+      getAnimate.style.left=pos+"px";
+    }
+  }
+
 }
