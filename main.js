908ecf1c55839e40ca2c9db2f7aa9e099f10bef7
jobayed24 <jkhouse117@gmail.com>
2023-08-29 01:33:10 +0600

    move ul li value to another ul list

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
+// move item second ul to mainul list
+let mainList=document.getElementById("mainList");
 
+let secondList=document.getElementById("secondList");
 
-    let getH1=document.getElementsByTagName("h1")[0];
-let getBlueButton=document.getElementById("blue");
-let getAttr;
-// add class attributes and add blue color by eventlistener
-getBlueButton.addEventListener("click",getAttr=function(){
-    let getAttr=document.createAttribute("class");
-    getAttr.value='blue';
-    getH1.setAttributeNode(getAttr);
-    
-    
-})
-//remove attributes node here or remoe by remove elements
-let getRemoveButton=document.getElementById("removeBlue");
-getRemoveButton.addEventListener('click',function(){
-    
-let getclassattr=getH1.removeAttribute("class")
-})
+let itemMove=document.getElementById("itemMove");
 
+itemMove.addEventListener('click',itemMoveFunction)
+
+function itemMoveFunction(){
+    let getSecondListValue=secondList.lastElementChild;
+    if(getSecondListValue){
+    mainList.appendChild(getSecondListValue)
+    }
+}
