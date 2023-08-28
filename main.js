3f4544208c90689fa4a8c74ca33133fc563b9a53
jobayed24 <jkhouse117@gmail.com>
2023-08-28 23:43:53 +0600

    add text color and remove color by js

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-let getBodyTag=document.body;
 
-let createPElement=document.createElement("p");
-let addText=document.createTextNode("Hey this is jobayed");
 
-createPElement.appendChild(addText);
+    let getH1=document.getElementsByTagName("h1")[0];
+let getBlueButton=document.getElementById("blue");
+let getAttr;
+// add class attributes and add blue color by eventlistener
+getBlueButton.addEventListener("click",getAttr=function(){
+    let getAttr=document.createAttribute("class");
+    getAttr.value='blue';
+    getH1.setAttributeNode(getAttr);
+    
+    
+})
+//remove attributes node here or remoe by remove elements
+let getRemoveButton=document.getElementById("removeBlue");
+getRemoveButton.addEventListener('click',function(){
+    
+let getclassattr=getH1.removeAttribute("class")
+})
 
-function addPElemnets(){
-  getBodyTag.appendChild(createPElement);
-}
-
-console.log(getBodyTag)
